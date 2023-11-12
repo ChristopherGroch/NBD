3738c2161c649c071697ebfa537cb1c294c44527
Jakub Świerczyński <jakswiercz@gmail.com>
2023-11-12 18:37:25 +0100

    dwa testy do mapperów

--- src/test/java/ReservationMapperTest.java
+++ src/test/java/ReservationMapperTest.java
@@ -1,3 +1,95 @@
+import Client.*;
+import Mappers.ReservationMapper;
+import Reservation.*;
+import Room.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReservationMapperTest {
+    private static ReservationMapper reservationMapper;
+
+    @BeforeAll
+    public static void setup(){
+        reservationMapper = new ReservationMapper();
+    }
+
+    @Test
+    public void ModelToMongoTest(){
+        Client client = new Client("J", "S", "37", new Standard());
+        Room room = new Room(5, 50, 500);
+        Reservation reservation = new Reservation(Reservation.ExtraBonus.A, 5, 7, LocalDateTime.of(2023,10,17,22,36), room, client);
+        ReservationMgd reservationMgd = reservationMapper.ModelToMongo(reservation);
+        assertEquals(ReservationMgd.class, reservationMgd.getClass());
+        assertEquals(reservation.getExtraBonus(), reservationMgd.getExtraBonus());
+        assertEquals(reservation.getGuestCount(), reservationMgd.getGuestCount());
+        assertEquals(reservation.getReservationDays(), reservationMgd.getReservationDays());
+        assertEquals(reservation.getBeginTime(), reservationMgd.getBeginTime());
+        assertEquals(reservation.getRoom().getRoomNumber(), reservationMgd.getRoomNumber());
+        assertEquals(reservation.getClient().getPersonalID(), reservationMgd.getClientPersonalID());
+    }
+
+    @Test
+    public void MongoToModelTest(){
+        ClientMgd client = new ClientMgd("J", "S", "37", new StandardMgd());
+        RoomMgd room = new RoomMgd(5, 50, 500);
+        ReservationMgd reservationMgd = new ReservationMgd(Reservation.ExtraBonus.A, 5, 7, LocalDateTime.of(2023,10,17,22,36), 5, "37");
+        Reservation reservation = reservationMapper.MongoToModel(reservationMgd, room, client);
+        assertEquals(Reservation.class, reservation.getClass());
+        assertEquals(reservationMgd.getExtraBonus(), reservation.getExtraBonus());
+        assertEquals(reservationMgd.getGuestCount(), reservation.getGuestCount());
+        assertEquals(reservationMgd.getReservationDays(), reservation.getReservationDays());
+        assertEquals(reservationMgd.getBeginTime(), reservation.getBeginTime());
+        assertEquals(reservationMgd.getRoomNumber(), reservation.getRoom().getRoomNumber());
+        assertEquals(reservationMgd.getClientPersonalID(), reservation.getClient().getPersonalID());
+    }
+
+    @Test
+    public void MongoToModelTerraceTest(){
+        ClientMgd client = new ClientMgd("J", "S", "37", new StandardMgd());
+        RoomWithTerraceMgd room = new RoomWithTerraceMgd(5, 50, 500,200);
+        ReservationMgd reservationMgd = new ReservationMgd(Reservation.ExtraBonus.A, 5, 7, LocalDateTime.of(2023,10,17,22,36), 5, "37");
+        Reservation reservation = reservationMapper.MongoToModel(reservationMgd, room, client);
+        assertEquals(Reservation.class, reservation.getClass());
+        assertEquals(reservationMgd.getExtraBonus(), reservation.getExtraBonus());
+        assertEquals(reservationMgd.getGuestCount(), reservation.getGuestCount());
+        assertEquals(reservationMgd.getReservationDays(), reservation.getReservationDays());
+        assertEquals(reservationMgd.getBeginTime(), reservation.getBeginTime());
+        assertEquals(reservationMgd.getRoomNumber(), reservation.getRoom().getRoomNumber());
+        assertEquals(reservationMgd.getClientPersonalID(), reservation.getClient().getPersonalID());
+    }
+
+    @Test
+    public void MongoToModelPoolTest(){
+        ClientMgd client = new ClientMgd("J", "S", "37", new StandardMgd());
+        RoomWithPoolMgd room = new RoomWithPoolMgd(5, 50, 500,200, 30, 7);
+        ReservationMgd reservationMgd = new ReservationMgd(Reservation.ExtraBonus.A, 5, 7, LocalDateTime.of(2023,10,17,22,36), 5, "37");
+        Reservation reservation = reservationMapper.MongoToModel(reservationMgd, room, client);
+        assertEquals(Reservation.class, reservation.getClass());
+        assertEquals(reservationMgd.getExtraBonus(), reservation.getExtraBonus());
+        assertEquals(reservationMgd.getGuestCount(), reservation.getGuestCount());
+        assertEquals(reservationMgd.getReservationDays(), reservation.getReservationDays());
+        assertEquals(reservationMgd.getBeginTime(), reservation.getBeginTime());
+        assertEquals(reservationMgd.getRoomNumber(), reservation.getRoom().getRoomNumber());
+        assertEquals(reservationMgd.getClientPersonalID(), reservation.getClient().getPersonalID());
+    }
+
+    @Test
+    public void MongoToModelRoomTest(){
+        Client client = new Client("J", "S", "37", new Standard());
+        Room room = new Room(5, 50, 500);
+        ReservationMgd reservationMgd = new ReservationMgd(Reservation.ExtraBonus.A, 5, 7, LocalDateTime.of(2023,10,17,22,36), 5, "37");
+        Reservation reservation = reservationMapper.MongoToModel(reservationMgd, room, client);
+        assertEquals(Reservation.class, reservation.getClass());
+        assertEquals(reservationMgd.getExtraBonus(), reservation.getExtraBonus());
+        assertEquals(reservationMgd.getGuestCount(), reservation.getGuestCount());
+        assertEquals(reservationMgd.getReservationDays(), reservation.getReservationDays());
+        assertEquals(reservationMgd.getBeginTime(), reservation.getBeginTime());
+        assertEquals(reservationMgd.getRoomNumber(), reservation.getRoom().getRoomNumber());
+        assertEquals(reservationMgd.getClientPersonalID(), reservation.getClient().getPersonalID());
+    }
 
 }
--- src/test/java/RoomMapperTest.java
+++ src/test/java/RoomMapperTest.java
@@ -1,2 +1,83 @@
+import Mappers.RoomMapper;
+import Room.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoomMapperTest {
+
+    private static RoomMapper roomMapper;
+
+    @BeforeAll
+    public static void setup() {
+        roomMapper = new RoomMapper();
+    }
+
+    @Test
+    public void ModelRoomToMongoTest(){
+        Room room = new Room(73, 500, 4);
+        RoomMgd roomMgd = roomMapper.ModelRoomToMongo(room);
+        assertEquals(RoomMgd.class, roomMgd.getClass());
+        assertEquals(room.getRoomNumber(), roomMgd.getRoomNumber());
+        assertEquals(room.getBasePricePerNight(), roomMgd.getBasePricePerNight());
+        assertEquals(room.getBedCount(), roomMgd.getBedCount());
+    }
+
+    @Test
+    public void ModelRoomWithPoolToMongoTest(){
+        RoomWithPool room = new RoomWithPool(73, 500, 4,50,20,30);
+        RoomWithPoolMgd roomMgd = roomMapper.ModelRoomToMongo(room);
+        assertEquals(RoomWithPoolMgd.class, roomMgd.getClass());
+        assertEquals(room.getRoomNumber(), roomMgd.getRoomNumber());
+        assertEquals(room.getBasePricePerNight(), roomMgd.getBasePricePerNight());
+        assertEquals(room.getPoolDepth(), roomMgd.getPoolDepth());
+        assertEquals(room.getPoolLength(), roomMgd.getPoolLength());
+        assertEquals(room.getPoolWidth(), roomMgd.getPoolWidth());
+    }
+
+    @Test
+    public void ModelRoomWithTerraceToMongoTest(){
+        RoomWithTerrace room = new RoomWithTerrace(73, 500, 4,50);
+        RoomWithTerraceMgd roomMgd = roomMapper.ModelRoomToMongo(room);
+        assertEquals(RoomWithTerraceMgd.class, roomMgd.getClass());
+        assertEquals(room.getRoomNumber(), roomMgd.getRoomNumber());
+        assertEquals(room.getBasePricePerNight(), roomMgd.getBasePricePerNight());
+        assertEquals(room.getTerraceSurface(), roomMgd.getTerraceSurface());
+    }
+
+    @Test
+    public void MongoRoomToModelTest(){
+        RoomMgd roomMgd = new RoomMgd(73, 500, 4);
+        Room room = roomMapper.MongoToModel(roomMgd);
+        assertEquals(Room.class, room.getClass());
+        assertEquals(roomMgd.getRoomNumber(), room.getRoomNumber());
+        assertEquals(roomMgd.getBasePricePerNight(), room.getBasePricePerNight());
+        assertEquals(roomMgd.getBedCount(), room.getBedCount());
+    }
+
+    @Test
+    public void MongoRoomWithPoolToModelTest(){
+        RoomWithPoolMgd roomMgd = new RoomWithPoolMgd(73, 500, 4,50,20,30);
+        RoomWithPool room = roomMapper.MongoToModel(roomMgd);
+        assertEquals(RoomWithPool.class, room.getClass());
+        assertEquals(roomMgd.getRoomNumber(), room.getRoomNumber());
+        assertEquals(roomMgd.getBasePricePerNight(), room.getBasePricePerNight());
+        assertEquals(roomMgd.getBedCount(), room.getBedCount());
+        assertEquals(roomMgd.getPoolDepth(), room.getPoolDepth());
+        assertEquals(roomMgd.getPoolLength(), room.getPoolLength());
+        assertEquals(roomMgd.getPoolWidth(), room.getPoolWidth());
+    }
+
+    @Test
+    public void MongoRoomWithTerraceToModelTest(){
+        RoomWithTerraceMgd roomMgd = new RoomWithTerraceMgd(73, 500, 4,50);
+        RoomWithTerrace room = roomMapper.MongoToModel(roomMgd);
+        assertEquals(RoomWithTerrace.class, room.getClass());
+        assertEquals(roomMgd.getRoomNumber(), room.getRoomNumber());
+        assertEquals(roomMgd.getBasePricePerNight(), room.getBasePricePerNight());
+        assertEquals(roomMgd.getBedCount(), room.getBedCount());
+        assertEquals(roomMgd.getTerraceSurface(), room.getTerraceSurface());
+    }
+
 }
